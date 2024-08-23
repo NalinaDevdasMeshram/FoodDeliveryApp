57dc6c72422b7445daf011b74cf2ccc62282a64d
nalina <naleenameshram85@gmail.com>
2024-08-23 19:10:55 +0530

    12haj

--- foodDels/src/Pages/Home/Home.jsx
+++ foodDels/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import "./Home.css";
-import Header from "../../Components/Headers/Header";
-import MenuExplore from "../../Components/MenuExplore/MenuExplore";
+import Header from "../../components/Headers/Header";
+import MenuExplore from "../../components/MenuExplore/MenuExplore";
 import { useState } from "react";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
